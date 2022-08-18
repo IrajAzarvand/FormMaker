1d8dc5df3d58044cf20b39c47b4263ac4bebfb01
Iraj <vazhenegar@hotmail.com>
2022-08-18 11:58:39 +0330

    change controller of form creator

--- app/Http/Controllers/FormCreatorController.php
+++ app/Http/Controllers/FormCreatorController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\TagType;
 use Illuminate\Http\Request;
 
-class FormCreator extends Controller
+class FormCreatorController extends Controller
 {
     public function LoadPge()
     {
